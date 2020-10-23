e0cadf02538889f7fb3dc09a1de38163b8eb8801
Malin Wedin Bäckman <malinwedinbackman@MacBook-Pro-3.lan>
2020-10-23 14:45:54 +0200

    add jsdoc comments to code

--- script.js
+++ script.js
@@ -1,17 +1,25 @@
-// start function when button is clicked..
+/**
+ * Will generate a random number and print it into the DOM.
+ */
 function printRandomNumber() {
     const randomNumber = getRandomNumber();
     addNumberToPage(randomNumber);
     //console.log('test')
 }
 
-// Helper function 1
+/**
+ * Generates a random number and returns it. 
+ * @returns {number} the random number that was generated 
+ */
 function getRandomNumber() {
     const randomNumber = Math.random() * 100
     return Math.round(randomNumber);
 }
 
-// Helper function 2 
+/**
+ * Places the passed in number into the DOM along with a comma and space
+ * @param {Number} number to be put into the DOM
+ */
 function addNumberToPage(number) {
     const div = document.getElementById('results')
     div.append(number + ', ');
