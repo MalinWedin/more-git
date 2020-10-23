6488982e43abe0e9ab6a5d132ca985714c222fe7
Malin Wedin Bäckman <malinwedinbackman@MacBook-Pro-3.lan>
2020-10-23 13:53:26 +0200

    add functions to random numbers

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
-function printRandomColor() {
-    console.log('test')
+// start function when button is clicked..
+function printRandomNumber() {
+    const randomNumber = getRandomNumber();
+    addNumberToPage(randomNumber);
+    //console.log('test')
+}
+
+// Helper function 1
+function getRandomNumber() {
+    const randomNumber = Math.random() * 100
+    return Math.random(randomNumber);
+}
+
+// Helper function 2 
+function addNumberToPage(number) {
+    console.log(number);
 }
